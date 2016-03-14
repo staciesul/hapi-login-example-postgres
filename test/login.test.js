15adf69181df38d1f154372488ddafe2f48b977c
nelsonic <contact.nelsonic+github@gmail.com>
2016-03-14 21:04:16 +0000

    implements test for un-registered email address. 100% coverage. :white_check_mark: :100:

--- test/login.test.js
+++ test/login.test.js
@@ -47,27 +47,59 @@ test(file+"/login without email (expect fail)", function(t) {
   });
 });
 
+test(file+"/login with correct email but incorrect password", function(t) {
+  // first register a new account
+  var email = 'dwyl.test+' + Math.floor(Math.random()*1000000)  + '@gmail.com';
+  console.log(email);
+
+  var options = {
+    method: "POST",
+    url: "/register",
+    payload : { email: email, password: 'pinkfluffyunicorns' }
+  };
+
+  server.inject(options, function(response) {
+    console.log(response.statusCode);
+    t.equal(response.statusCode, 200, "Registration succeeded for: "+email);
+    options.url = '/login'; // now login
+    options.payload.password = 'incorrect';
+    server.inject(options, function(response) {
+      t.equal(response.statusCode, 401, "Login Faild (incorrect password)");
+      t.end();
+    });
+  });
+});
+
+test(file+"/login with unregistered email address", function(t) {
+  var email = 'dwyl.test+' + Math.floor(Math.random()*1000000)  + '@gmail.com';
+  var options = {
+    method: "POST",
+    url: "/login",
+    payload : { email: email, password: 'pinkfluffyunicorns' }
+  }; // this user is NOT registered expect
+  server.inject(options, function(response) {
+    t.equal(response.statusCode, 401, "Login Faild (email not registered)");
+    t.end();
+  });
+
+});
+
+
 test(file+"/login With Valid Data (Success Test)", function(t) {
   // first register a new account
-  var email = 'dwyl.test+' + Math.floor(Math.random()*100)  + '@gmail.com';
-  console.log(email);
+  var email = 'dwyl.test+' + Math.floor(Math.random()*1000000)  + '@gmail.com';
+  // console.log(email);
   var options = {
     method: "POST",
     url: "/register",
     payload : { email: email, password: 'supersecret' }
   };
-
-  // var login_options = {
-  //   method: "POST",
-  //   url: "/login",
-  //   payload : { email: test_email, password: 'supersecret' }
-  // };
   server.inject(options, function(response) {
     console.log(response.statusCode);
+    t.equal(response.statusCode, 200, "Registration succeeded for: "+email);
     options.url = '/login'; // now login
     server.inject(options, function(response) {
-      console.log(response.result)
-      t.equal(response.statusCode, 200, "Great Success!");
+      t.equal(response.statusCode, 200, "Login Succeeded!");
       t.end();
     });
   });
