ceb2ee0cd71c8fb6ec67d04dbfb45b912c73def9
nelsonic <contact.nelsonic+github@gmail.com>
2016-03-14 22:31:44 +0000

    tidy up project files into logical sub directories. remove redundant files :recycle:

--- test/_create_table.test.js
+++ test/_create_table.test.js
@@ -11,7 +11,7 @@ function create_tables (callback) {
   var client = new pg.Client(process.env.DATABASE_URL);
   client.connect(function(err) {
     assert(!err); // if db connection fails then EXPLODE!!
-    var file = require('path').resolve(__dirname + '/../database_setup.sql');
+    var file = require('path').resolve(__dirname + '/database_setup.sql');
     var query = require('fs').readFileSync(file, 'utf8').toString();
     console.log('\n', query);
     client.query(query, function(err, result) {
