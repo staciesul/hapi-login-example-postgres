4ed731fd0951093f4f471ca91492303cdac502b9
nelsonic <contact.nelsonic+github@gmail.com>
2016-03-20 17:01:10 +0000

    remove example folder. no longer used

--- example/model.js
+++
@@ -1,6 +0,0 @@
-var Joi = require('joi');
-var custom_fields = {
-  email     : Joi.string().email().required(), // Required
-  password  : Joi.string().required().min(6)   // minimum length 6 characters
-};
-module.exports = custom_fields;
