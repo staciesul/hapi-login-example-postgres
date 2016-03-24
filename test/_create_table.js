dbf07eecef2932188f3b832b0fc6031f8ca262de
nelsonic <contact.nelsonic+github@gmail.com>
2016-03-24 07:48:54 +0000

    updating to the latest version of hapi-postgres-connection solved the issue. :wink:

--- test/_create_table.js
+++ test/_create_table.js
@@ -1,5 +1,5 @@
 require('env2')('./config.env'); // see: https://github.com/dwyl/env2
-
+var assert = require('assert');
 // avoid having pg as a dependency by simply requiring the nested dependcy
 var _pg = '../node_modules/hapi-postgres-connection/node_modules/pg/lib/index.js';
 var pg = require(_pg); // manual connection just for this
@@ -13,6 +13,7 @@ function create_tables (callback) {
     console.log('\n', query);
     client.query(query, function(err, result) {
       client.end(); // close connection to database
+      require('decache')(_pg);
       return callback(err, result);
     });
   });
